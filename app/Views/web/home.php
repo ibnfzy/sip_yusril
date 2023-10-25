442e09905a441fafc6f008e242128f8ec5f7b853
Ibnu Fauzy <58937373+ibnfzy@users.noreply.github.com>
2023-10-25 18:13:52 +0800

    yes

--- app/Views/web/home.php
+++ app/Views/web/home.php
@@ -3,23 +3,25 @@
 <?= $this->section('content'); ?>
 
 <!-- Start Slider -->
-<div id="slides-shop" class="cover-slides">
-  <ul class="slides-container">
-    <?php foreach ($corousel as $item): ?>
-    <li class="text-left">
-      <img src="<?= base_url('uploads/' . $item['gambar']); ?>" alt="">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12">
-            <!-- <h1 class="m-b-20"><strong>Welcome To <br> Thewayshop</strong></h1>
-            <p class="m-b-40">See how your users experience your website in realtime or view <br> trends to see any
-              changes in performance over time.</p>
-            <p><a class="btn hvr-hover" href="#">Shop New</a></p> -->
+<div id="slides-shop" class="cover-slides" style="max-height: 500px;">
+  <ul class="slides-container" style="max-height: 500px;">
+    <div class="container-fluid">
+      <?php foreach ($corousel as $item): ?>
+        <li class="">
+          <img style="max-height: 500px; max-width: 500px" src="<?= base_url('uploads/' . $item['gambar']); ?>" alt="">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <!-- <h1 class="m-b-20"><strong>Welcome To <br> Thewayshop</strong></h1>
+                  <p class="m-b-40">See how your users experience your website in realtime or view <br> trends to see any
+                    changes in performance over time.</p>
+                  <p><a class="btn hvr-hover" href="#">Shop New</a></p> -->
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </li>
-    <?php endforeach ?>
+        </li>
+      <?php endforeach ?>
+    </div>
   </ul>
   <div class="slides-navigation">
     <a href="#" class="next"><i class="fa fa-angle-right" aria-hidden="true"></i></a>
@@ -70,32 +72,32 @@
 
     <div class="row special-list">
       <?php foreach ($rekom as $r): ?>
-      <div class="col-lg-3 col-md-6 special-grid best-seller">
-        <div class="products-single fix">
-          <div class="box-img-hover">
-            <div class="type-lb">
-              <!-- <p class="sale">Sale</p> -->
+        <div class="col-lg-3 col-md-6 special-grid best-seller">
+          <div class="products-single fix">
+            <div class="box-img-hover">
+              <div class="type-lb">
+                <!-- <p class="sale">Sale</p> -->
+              </div>
+              <img src="<?= base_url('uploads/' . $r['gambar']); ?>" class="img-fluid" alt="Image">
+              <div class="mask-icon">
+                <ul>
+                  <li><a href="<?= base_url('Detail/' . $r['id_barang']); ?>" data-toggle="tooltip" data-placement="right"
+                      title="View"><i class="fas fa-eye"></i></a>
+                  </li>
+                </ul>
+                <a class="cart" href="<?= base_url('add_barang/' . $r['id_barang']); ?>">Add to Cart</a>
+              </div>
             </div>
-            <img src="<?= base_url('uploads/' . $r['gambar']); ?>" class="img-fluid" alt="Image">
-            <div class="mask-icon">
-              <ul>
-                <li><a href="<?= base_url('Detail/' . $r['id_barang']); ?>" data-toggle="tooltip" data-placement="right"
-                    title="View"><i class="fas fa-eye"></i></a>
-                </li>
-              </ul>
-              <a class="cart" href="<?= base_url('add_barang/' . $r['id_barang']); ?>">Add to Cart</a>
+            <div class="why-text">
+              <h4>
+                <?= $r['nama_barang']; ?>
+              </h4>
+              <h5> Rp.
+                <?= number_format($r['harga'], 0, ',', '.'); ?>
+              </h5>
             </div>
-          </div>
-          <div class="why-text">
-            <h4>
-              <?= $r['nama_barang']; ?>
-            </h4>
-            <h5> Rp.
-              <?= number_format($r['harga'], 0, ',', '.'); ?>
-            </h5>
           </div>
         </div>
-      </div>
       <?php endforeach ?>
 
 
