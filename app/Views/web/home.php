5ceeeac10fb569b4f4d22c8b7fc1b2f47a0d9ddd
Ibnu Fauzy <58937373+ibnfzy@users.noreply.github.com>
2023-11-04 13:15:22 +0800

    fix

--- app/Views/web/home.php
+++ app/Views/web/home.php
@@ -6,9 +6,9 @@
 <div id="slides-shop" class="cover-slides" style="">
   <ul class="slides-container" style="">
     <?php foreach ($corousel as $item): ?>
-      <li class="">
-        <img style="" src="<?= base_url('uploads/' . $item['gambar']); ?>" alt="">
-      </li>
+    <li class="">
+      <img style="" src="<?= base_url('uploads/' . $item['gambar']); ?>" alt="">
+    </li>
     <?php endforeach ?>
   </ul>
   <div class="slides-navigation">
@@ -22,21 +22,13 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-6">
-        <h2 class="noo-sh-title">We are <span>ThewayShop</span></h2>
-        <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-          laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto
-          beatae vitae dicta sunt explicabo. Nemo enim ipsam
-          voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-          eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia
-          dolor sit amet, consectetur, adipisci velit,
-          sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-          voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit
-          laboriosam, nisi ut aliquid ex ea commodi consequatur?
-          Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae
-          consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-          laboris nisi ut aliquip ex ea commodo consequat.
+        <h2 class="noo-sh-title">Selamat Datang di Website Toko Basseang Elektronik</h2>
+        <p>
+          Toko Basseang Elektronik yang berlokasi di Kabupaten Polewali Mandar adalah tempat yang sangat lengkap bagi
+          para konsumen yang menginginkan berbagai jenis perangkat elektronik untuk berbagai keperluan. Dari kipas angin
+          yang menyegarkan, lampu LED yang hemat energi, charger handphone yang kompatibel dengan berbagai merek, hingga
+          speaker berkualitas tinggi dan pilihan handphone terbaru yang memenuhi kebutuhan komunikasi Anda, toko ini
+          menyediakan berbagai opsi yang sangat beragam untuk memenuhi segala kebutuhan elektronik Anda.
         </p>
       </div>
       <!-- <div class="col-lg-6">
@@ -60,23 +52,23 @@
 
     <div class="row special-list">
       <?php foreach ($rekom as $r): ?>
-        <div class="col-lg-3 col-md-6 special-grid best-seller">
-          <div class="products-single fix">
-            <a href="<?= base_url('Detail/' . $r['id_barang']); ?>">
-              <img src="<?= base_url('uploads/' . $r['gambar']); ?>" class="img-fluid" alt="Image">
-            </a>
-          </div>
-          <div class="why-text">
-            <a href="<?= base_url('Detail/' . $r['id_barang']); ?>">
-              <h4>
-                <?= $r['nama_barang']; ?>
-              </h4>
-            </a>
-            <h5> Rp.
-              <?= number_format($r['harga'], 0, ',', '.'); ?>
-            </h5>
-          </div>
+      <div class="col-lg-3 col-md-6 special-grid best-seller">
+        <div class="products-single fix">
+          <a href="<?= base_url('Detail/' . $r['id_barang']); ?>">
+            <img src="<?= base_url('uploads/' . $r['gambar']); ?>" class="img-fluid" alt="Image">
+          </a>
         </div>
+        <div class="why-text">
+          <a href="<?= base_url('Detail/' . $r['id_barang']); ?>">
+            <h4>
+              <?= $r['nama_barang']; ?>
+            </h4>
+          </a>
+          <h5> Rp.
+            <?= number_format($r['harga'], 0, ',', '.'); ?>
+          </h5>
+        </div>
+      </div>
       <?php endforeach ?>
 
 
